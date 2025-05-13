7afdaefb39ac74c93e0288507cb9cbd586d0da67
Andis Draguns <andis.draguns@gmail.com>
2025-05-13 17:41:44 +0000

    Added matrix grid visualization

--- circuits/visualize.py
+++ circuits/visualize.py
@@ -7,18 +7,21 @@ import matplotlib.colors as mcolors
 from matplotlib.figure import Figure
 from typing import Literal, Any
 
+Matrix = t.Tensor | list[list[int]] | list[list[float]]
+
 
 @dataclass
 class MatrixPlot:
     """Visualize a 2D matrix"""
 
-    init_m: t.Tensor | list[list[int]] | list[list[float]]
+    init_m: Matrix
     scale: float = 0.01
     clip_val: float | int = 20
     raster: bool = False
     quick: bool = False
     downsample_factor: int = 16
     downsample_kernel: Literal["mean", "median", "max_abs"] = "max_abs"
+    square_size: int | None = None
     m: t.Tensor = field(default_factory=lambda: t.Tensor([[0, 0], [0, 0]]), init=False)
 
     def __post_init__(self) -> None:
@@ -63,6 +66,7 @@ class MatrixPlot:
         s = self.scale
         if self.raster:
             s *= 2  # account for Image retina display option
+
         fig, ax = plt.subplots(figsize=(w * s, h * s))  # type: ignore[reportUnknownMemberType]
         fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
         norm = mcolors.SymLogNorm(
@@ -76,20 +80,28 @@ class MatrixPlot:
             pcm.set_edgecolor("none")  # removes the edgecolors='face' distortion
             ax.set_anchor("NW")
             ax.axis("off")
+            if self.square_size is not None:
+                fig.set_size_inches(self.square_size, self.square_size)
             return fig
 
-    def draw(self) -> None:
-        """Display a figure as raster or vector graphics"""
+    def get_buffer(self) -> BytesIO:
         fig = self.get_matrix_figure()
-        if self.quick:
-            fig.show()  # quick raster without transparency
-            return
         buffer = BytesIO()
         format = "png" if self.raster else "svg"
         plt.savefig(buffer, format=format, transparent=True, bbox_inches="tight")  # type: ignore[reportUnknownMemberType]
         buffer.seek(0)
         plt.close(fig)
-        self.load_and_draw(buffer)
+        return buffer
+
+    def draw(self) -> None:
+        """Display a figure as raster or vector graphics"""
+        if self.quick:  # opaque raster
+            fig = self.get_matrix_figure()
+            fig.show()
+            return
+        else:
+            buffer = self.get_buffer()
+            self.load_and_draw(buffer)
 
     def save(self, filename: str) -> None:
         fig = self.get_matrix_figure()  # type: ignore[reportUnknownMemberType]
@@ -97,19 +109,34 @@ class MatrixPlot:
         plt.savefig(filename, format=format, transparent=True, bbox_inches="tight")  # type: ignore[reportUnknownMemberType]
 
     def load_and_draw(self, file: str | BytesIO) -> None:
-        try:
-            from IPython.display import SVG, Image, display  # type: ignore[reportUnknownMemberType]
+        "Load and draw in IPython"
+        from IPython.display import SVG, Image, display  # type: ignore[reportUnknownMemberType]
+        if self.raster:
+            data = file if isinstance(file, str) else file.getvalue()
+            display(Image(data, retina=True))
+        else:
+            data = (
+                file if isinstance(file, str) else file.getvalue().decode("utf-8")
+            )
+            display(SVG(data))
 
-            if self.raster:
-                data = file if isinstance(file, str) else file.getvalue()
-                display(Image(data, retina=True))
-            else:
-                data = (
-                    file if isinstance(file, str) else file.getvalue().decode("utf-8")
-                )
-                display(SVG(data))
-        except:
-            raise ValueError("load_and_draw is implemented only in IPython")
+
+def get_matrix_grid_html(matrices: list[Matrix], gap:int=5, **kwargs: Any) -> str:
+    "For displaying matrices in a flexible grid. For use with IPython.display.HTML"
+    import base64
+    css = f"<style>.matrix-container {{display: flex; flex-wrap: wrap; gap: {gap}px;}}</style>"
+    matrices_html: list[str] = []
+    for m in matrices:
+        mplot = MatrixPlot(m, **kwargs)
+        buffer_val = mplot.get_buffer().getvalue()
+        if mplot.raster:
+            img_str = base64.b64encode(buffer_val).decode('utf-8')
+            html_str = f'<img src="data:image/png;base64,{img_str}" />'
+        else:
+            html_str = buffer_val.decode('utf-8')
+        matrices_html.append(html_str)
+    html = f"{css}<div class='matrix-container'>{''.join(matrices_html)}</div>"
+    return html
 
 
 def draw(m: t.Tensor | list[list[int]] | list[list[float]], **kwargs: Any) -> None:
@@ -118,7 +145,9 @@ def draw(m: t.Tensor | list[list[int]] | list[list[float]], **kwargs: Any) -> No
         MatrixPlot(m, **kwargs).draw()
 
 
-# Example:
-# plot_kwargs = {'scale': 0.01, 'clip_val': 20, 'raster': False, 'quick': False,
-#                 'downsample_factor': 16, 'downsample_kernel': 'max_abs'}
-# draw(t.randn(1000, 1000), **plot_kwargs)
+## Example:
+# plot_kwargs = {'scale': 0.1, 'clip_val': 20, 'raster': True, 'quick': False,
+#                 'downsample_factor': 16, 'downsample_kernel': 'max_abs', 'square_size': 10}
+# m = t.randn(20, 10)
+# draw(m, **plot_kwargs)
+# MatrixPlot(m, **plot_kwargs).save('test.png')
